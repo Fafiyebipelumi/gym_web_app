0c59767c9354dbf2ae5e1968c72f6e173468247d
Oluwapelumi <fafyeepelumi04@gmail.com>
2022-06-23 02:02:58 -0700

    latest-commit

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -10,3 +10,16 @@
 
 // export default Loader;
 
+import React from 'react';
+import { Stack } from '@mui/material';
+import ReactLoading from 'react-loading';
+
+const Loader = () => {
+    return (
+        <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+            <ReactLoading type='spin' color='black' delay={5000} width={'4%'} />
+        </Stack>
+    )
+}
+
+export default Loader;
--- src/components/SimiliarExercises.js
+++ src/components/SimiliarExercises.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
-// import Loader from './Loader';
+import Loader from './Loader';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
     <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
@@ -10,7 +10,7 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
             Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
         </Typography>
         <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
-            {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : 'Loading...'}
+            {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
         </Stack>
         <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
             Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
